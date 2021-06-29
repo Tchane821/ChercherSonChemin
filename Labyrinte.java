a53ddf0589e00b288103dc623720866f9135b8e4
Tchane821 <googldu59@gmail.com>
2021-06-29 10:23:33 +0200

    init lab + proj = done

--- Labyrinte.java
+++ Labyrinte.java
@@ -1,46 +1,77 @@
-import javax.print.DocFlavor;
-        import java.lang.reflect.Array;
-
 public class Labyrinte {
 
     private char[][] laby;
-    private int h; private int l;
+    private int hauteur;
+    private int largeur;
     private int[] start;
     private int[] arriver;
+    private final char mure = 'X';
+    private final char vide = 'o';
 
     public Labyrinte(int hauteur, int largeur) {
-        this.h = hauteur; this.l = largeur;
+        this.hauteur = hauteur;
+        this.largeur = largeur;
         this.laby = new char[hauteur][largeur];
         generate();
     }
 
     private void generate() {
-        if(h > 2 && l > 3){
+        if (hauteur > 2 && largeur > 3) {
             startPoint();
             endPoint();
             tracerLeChemin();
-        }else{
+        } else {
             fillpaterne();
         }
     }
 
-    private void startPoint(){
-        start = new int[]{h/2,0};
+    private void startPoint() {
+        start = new int[]{hauteur / 2, 0};
     }
 
-    private void endPoint(){
-        arriver = new int[]{h/2,l};
+    private void endPoint() {
+        arriver = new int[]{hauteur / 2, largeur-1};
     }
 
-    private void tracerLeChemin(){
-
+    private void tracerLeChemin() {
+        initLabyrinte();
     }
 
-    private void fillpaterne(){
-        for (int i = 0; i < h ; i++) {
-            for (int j = 0; j < l; j++) {
-                laby[i][l] = 'X';
+    private void initLabyrinte() {
+        for (int i = 0; i < hauteur; i++) {
+            for (int j = 0; j < largeur; j++) {
+                laby[i][j] = vide;
             }
         }
+        for (int k = 0; k < largeur; k++) {
+            laby[0][k] = 'X';
+            laby[hauteur - 1][k] = mure;
+        }
+        for (int g = 0; g < hauteur; g++) {
+            laby[g][0] = mure;
+            laby[g][largeur - 1] = mure;
+        }
+        laby[start[0]][start[1]] = vide;
+        laby[arriver[0]][arriver[1]] = vide;
+    }
+
+    private void fillpaterne() {
+        for (int i = 0; i < hauteur; i++) {
+            for (int j = 0; j < largeur; j++) {
+                laby[i][j] = mure;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String res = "";
+        for (char[] l : laby) {
+            for (char c : l) {
+                res += "| " + c + " ";
+            }
+            res += "|\n";
+        }
+        return res;
     }
 }
