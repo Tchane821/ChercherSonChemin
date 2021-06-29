2669f41dc26d44858b92af5d9cba0b432edabce0
Tchane821 <googldu59@gmail.com>
2021-06-29 15:42:57 +0200

    move

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Labyrinte L = new Labyrinte(11, 11,30);
-        System.out.println(L.toString());
-        TrouveSonChemin tsc = new TrouveSonChemin(L);
-        tsc.decouvre();
-    }
-}
